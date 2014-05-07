d661ddee7179a58679258ba8ccb557a7fcf60a74
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2014-05-07 17:28:09 -0500

    wired 'add miner button' to add miners

---
+++ src/app/mine.js
@@ -0,0 +1,34 @@
+App.MineModel = Ember.Object.extend({
+	gems: null,
+	globals: null,	
+});
+
+App.MineRoute = Ember.Route.extend({
+	model: function(){
+		var model = App.MineModel.create();
+
+		var store = this.get('store');
+
+		// model.gems = store.find('gem');
+		model.globals = store.find('globals', 1);
+
+		return model;
+	}
+});
+
+App.MineController = Ember.ObjectController.extend({
+	actions:{
+		addMiner: function(){
+			var store = this.get('store');
+			var globals = store.find('globals', 1).then(function(record){
+				var miners = record.get('miners');
+				record.set('miners', miners+1);
+				record.save();
+			});
+		},
+		
+		advanceMonth: function(){
+			
+		},
+	}
+});
