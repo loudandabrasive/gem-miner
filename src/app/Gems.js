3a1122cc4510f55b95749bd230118486ed3ef987
Patrick McVeety-Mill <p.mcvtm@gmail.com>
2014-05-07 20:19:04 -0500

    added action to sell all gems

--- src/app/Gems.js
+++ src/app/Gems.js
@@ -11,5 +11,28 @@ App.GemsRoute = Ember.Route.extend({
 });
 
 App.GemsController = Ember.ArrayController.extend({
+	actions:{
+		sellAll: function(){
+			var store = this.get('store');
+			var payLoad = store.find('gem').then(function(gems){
+				var worth = 0;
+				console.log(gems);
+				gems.forEach(function(gem, index){
+					worth += gem.get('value');
+					gem.deleteRecord();
+					gem.save();
+				});
 
+				return worth;
+			});
+
+			payLoad.then(function(worth){
+				var confing = store.find('globals', 1).then(function(record){
+					var money = record.get('money');
+					record.set('money', money+worth);
+					record.save();
+				});
+			});
+		},
+	}
 });
